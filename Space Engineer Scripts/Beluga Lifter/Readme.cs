3dc99eb8f058b311881acee614d51cd707fd7d2d
Joshua N Pope-Lewis <Joshua.pope-lewis@networkrail.co.uk>
2018-03-17 23:28:06 +0000

    Read me - Initial upload - JPL

--- Space Engineer Scripts/Beluga Lifter/Readme.cs
+++ Space Engineer Scripts/Beluga Lifter/Readme.cs
@@ -1,8 +1,7 @@
-﻿/*
- *   R e a d m e
- *   -----------
- * 
- *   In this file you can include any instructions or other comments you want to have injected onto the 
- *   top of your final script. You can safely delete this file if you do not want any such comments.
- * 
- */
+/* Beluga Lifter Controller V1
+/* This script is functional
+/* Ability to Launch/Climb/Decend/Land the Beluga Lifter Rocket
+/* Few minor issues to overcome but still functions as required
+/* Blargs Cruise Control is required!
+
+/*V2 is in the works, self-written Cruise script, more Terminal Commands, relay flight info to a “Control Center”, remove need for all Timer Blocks!
