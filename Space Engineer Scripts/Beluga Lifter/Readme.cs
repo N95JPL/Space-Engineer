62faa9759b69e723df44036784c6f7bc78edb3b9
Joshua N Pope-Lewis <joshua.pope-lewis@networkrail.co.uk>
2018-03-18 22:10:11 +0000

    Created Cruise Control Project - JPL Minor bug fixes - JPL

--- Space Engineer Scripts/Beluga Lifter/Readme.cs
+++ Space Engineer Scripts/Beluga Lifter/Readme.cs
@@ -1,7 +1,7 @@
-/* Beluga Lifter Controller V1
-/* This script is functional
-/* Ability to Launch/Climb/Decend/Land the Beluga Lifter Rocket
-/* Few minor issues to overcome but still functions as required
-/* Blargs Cruise Control is required!
+/* Beluga Lifter Controller V1 */
+/* This script is functional */
+/* Ability to Launch/Climb/Decend/Land the Beluga Lifter Rocket */
+/* Few minor issues to overcome but still functions as required */
+/* Blargs Cruise Control is required! */
 
-/*V2 is in the works, self-written Cruise script, more Terminal Commands, relay flight info to a “Control Center”, remove need for all Timer Blocks!
+/*V2 is in the works, self-written Cruise script, more Terminal Commands, relay flight info to a “Control Center”, remove need for all Timer Blocks! */
