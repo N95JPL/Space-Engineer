a7d40bdfb6709a7b20652bfac67b515bb127cc72
Joshua Pope-Lewis <joshua.pope-lewis@outlook.com>
2018-04-21 01:17:04 +0100

    JPL

--- Drawing Lib/Readme.cs
+++
@@ -1,6 +0,0 @@
-﻿/*
- *   R e a d m e
- *   -----------
- *  WORKSHOP LINK: https://steamcommunity.com/sharedfiles/filedetails/?id=1366249820
- * 
- */
--- Released/Drawing Lib/script.cs
+++ Released/Drawing Lib/script.cs
@@ -1,12 +1,4 @@
-﻿/*
- *   R e a d m e
- *   -----------
- * 
- *   In this file you can include any instructions or other comments you want to have injected onto the 
- *   top of your final script. You can safely delete this file if you do not want any such comments.
- * 
- */
-
+﻿
 //Example Vars - Delete when not using example script at bottom!
 string Ship = "Beluga Lifter";
 int currentAltitude = 0;
